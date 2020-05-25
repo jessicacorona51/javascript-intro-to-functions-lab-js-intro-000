66f84440c6675adb90dd8d4447d6492785a413a5
Jessica Corona <jessicacorona51@yahoo.com>
2020-05-25 20:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ function whisper (string) {
   return string.toLowerCase()
 }
 function logShout(string){
-  uppercase=string.toUpperCase()
+  uppercase= string.toUpperCase()
   string=uppercase;
   console.log (uppercase);
 }
 function logWhisper(string){
-  lowercase=string.toLowerCase()
+  lowercase= string.toLowerCase()
   string=lowercase;
   console.log (lowercase);
+}
+function sayHiToGrandma (string) {
+  if (string.toLowerCase() === string){
+    return “I can’t hear you!”;
+  } 
+  if (string.toUpperCase() ===string) {
+    return “YES INDEED!”;
+  } 
+  if (string === ‘I love you, Grandma.’) {
+    return “I love you, too.”;
+}
 }
